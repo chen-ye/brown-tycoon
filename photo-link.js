87604c05d128550dfafc8c2da5500fd47ae7721b
= <nateparro2t@gmail.com>
2015-12-19 01:52:35 -0500

    photo link

---
+++ photo-link.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('img[src]').each(function(i, el) {
+		var src = $(el).attr('src');
+		$(el).click(function() {
+			if ($(el).closest('[data-disable-photo-link]').length == 0) {
+				var url = 'image.html?img=' + encodeURIComponent(src);
+				window.open(url, '_blank');
+			}
+		})
+	})
+})
