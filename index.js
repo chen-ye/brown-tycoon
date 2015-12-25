b7ff2df0d6498cf010480457eaced3336f7a687c
= <nateparro2t@gmail.com>
2015-12-25 06:41:06 -0500

    content

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 function createImageFlows() {
-	$('.image-flow').bind('mousemove touchmove', function(e) {
-		var self = this;
-		var children = $(self).children('div');
-		var offset = $(self).offset(); 
+	$('.image-flow > div').each(function(i, div) {
+		$(div).find('p').prepend($("<strong></strong>").text((i+1) + '. ').addClass('number'));
+	})
+	$('.image-flow').each(function(_, flow) {
+		var children = $(flow).children('div').addClass('page');
 		
-		var x;
-		if (e.originalEvent.touches){
-			x = e.originalEvent.touches[0].pageX;
-		} else if (e.pageX) {
-			x = e.pageX;
-		}
+		// create progress bar:
+		var barContainer = $("<div></div>").addClass('progress-bar-container').appendTo(flow);
+		var bar = $("<div></div>").addClass('progress-bar').appendTo(barContainer).css('width', (1 * 100 / children.length) + '%');
 		
-		var relX = x - offset.left;
-		var i = Math.max(0, Math.min(children.length-1, Math.round(-0.5 + relX / $(self).width() * children.length)));
-		children.hide().eq(i).show();
-	}).bind('touchstart', function(e) {
-		//e.preventDefault();
-	});
+		$(flow).bind('mousemove touchmove', function(e) {
+			var self = this;
+			var offset = $(self).offset(); 
+		
+			var x;
+			if (e.originalEvent.touches){
+				x = e.originalEvent.touches[0].pageX;
+			} else if (e.pageX) {
+				x = e.pageX;
+			}
+		
+			var relX = x - offset.left;
+			var i = Math.max(0, Math.min(children.length-1, Math.round(-0.5 + relX / $(self).width() * children.length)));
+			children.hide().eq(i).show();
+			$(bar).css('width', ((i+1) * 100 / children.length) + '%');
+		}).bind('touchstart', function(e) {
+			//e.preventDefault();
+		});
+	})
 }
 
 $(document).ready(function() {
--- photo-link.js
+++ photo-link.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 		$(el).click(function() {
 			if ($(el).closest('[data-disable-photo-link]').length == 0) {
 				var url = 'image.html?img=' + encodeURIComponent(src);
-				window.open(url, '_blank');
+				// window.open(url, '_blank');
+				location.href = url;
 			}
 		})
 	})
